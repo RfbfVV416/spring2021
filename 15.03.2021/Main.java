be7d0af3ec81873b6829160d873ba50079408e4a
Alyona Bebeshko <myworkmail01@mail.ru>
2021-03-15 19:10:26 +0400

    static counter

--- 15.03.2021/Main.java
+++ 15.03.2021/Main.java
@@ -3,22 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Count x = new Count();
-        x.displayCount();
-        Count y = new Count();
-        y.displayCount();
+        Count.displayCount();
+        Count.displayCount();
+        //Count x = new Count();
+        //x.displayCount();
+        //Count y = new Count();
+        //y.displayCount();
     }
 
 
 }
 class Count {
     static int counter = 0;
-
     public Count() {
-        counter++;
     }
-
-    public void displayCount() {
+    static public void displayCount() {
+        counter++;
         System.out.println(counter);
     }
 }
