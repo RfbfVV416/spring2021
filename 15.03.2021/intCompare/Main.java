c65691e21b4211e02cfbe01748a5c460eb42a15d
Alyona Bebeshko <myworkmail01@mail.ru>
2021-03-15 19:48:05 +0400

    intCompare

---
+++ 15.03.2021/intCompare/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Integer integer1 = new Integer(1);
+        Integer integer2 = new Integer(1);
+
+        if (integer1.equals(integer2)) System.out.println("равны");
+        else System.out.println("не равны");
+
+        int integerUnbox1 = integer1;
+        int integerUnbox2 = integer2;
+        if (integerUnbox1 == integerUnbox2) System.out.println("равны");
+        else System.out.println("не равны");
+
+    }
+
+
+}
+
