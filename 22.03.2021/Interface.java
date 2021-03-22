1671d8107acc102bb3fe4fcbc90c3761d8cc06fd
Alyona Bebeshko <myworkmail01@mail.ru>
2021-03-22 19:39:03 +0400

    interface

---
+++ 22.03.2021/Interface.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+interface Movable{
+   void move();
+   void fill();
+}
+
+class Car implements Movable{
+    public void move(){
+        System.out.println("Car is riding");
+    }
+    public void fill(){
+        System.out.println("You filled up the car");
+    }
+}
+
+class Plane implements Movable{
+    public void move(){
+        System.out.println("Plane is flying");
+    }
+    public void fill(){
+        System.out.println("You filled up the plane");
+    }
+}
+
+
+
+
