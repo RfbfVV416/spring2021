c6990fb5d380346954a2990d457652fb124a5965
Alyona Bebeshko <myworkmail01@mail.ru>
2021-04-02 13:46:10 +0400

    garbage

---
+++ 26.03.2021/homework/Garbage.java
@@ -0,0 +1,23 @@
+package albe;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String command;
+        while (true) {
+            command = in.nextLine();
+            if (command.trim().equals("true")) {
+                while (true) {
+                    String a = "123";
+                    String b = new String("123");
+                    String c = null;
+                    a = c;
+                    b = c;
+                }
+            }
+        }
+    }
+}
+
+
